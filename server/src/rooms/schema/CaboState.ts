f49c489b5d481187d171eaad4c85ce82c6e47a21
MaayanRytwo <maayanRytwo@gmail.com>
2021-02-21 15:11:49 +0200

    add properties to the state

--- server/src/rooms/schema/CaboState.ts
+++ server/src/rooms/schema/CaboState.ts
@@ -1,17 +1,21 @@
 import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
 
 export class CaboState extends Schema {
+  
+  @type("string")
+  currentTurn:string = "";
 
   @type({map: Player})
-  players=new MapSchema<Player>();
+  players:MapSchema<Player>=new MapSchema<Player>();
 
   @type("number")
   numOfPlayer:number;
 
-  @type("string")
-  currentTurn = "";
-
   @type([Card])
-  discard_pile=new ArraySchema<Card>();
+  discard_pile:ArraySchema<Card> =new ArraySchema<Card>();
+  
+  @type(CardPack)
+  pack:CardPack=new CardPack();
+
 
 }
