6326b10fa22b40f330a11302340c86791002f17b
AlexV <alexv1591@gmail.com>
2021-02-22 19:57:19 +0200

    added how-to-play component and set up router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HowToPlayComponent } from './how-to-play/how-to-play.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'how-to-play', component: HowToPlayComponent },
+  { path: '**', component: MenuComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ MenuComponent, HowToPlayComponent ]
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { ChatComponent } from './chat/chat.component';
-import { MenuComponent } from './menu/menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
     ChatComponent,
-    MenuComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
