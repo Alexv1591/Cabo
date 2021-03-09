4527fb88073bbeb7623380e7b0038effe50cc3b6
MaayanRytwo <maayanRytwo@gmail.com>
2021-03-09 18:38:40 +0200

    refactor Player and Hand so they be suitable for inheritance

--- server/lib/Player.ts
+++ server/lib/Player.ts
@@ -6,45 +6,45 @@ import { Card } from "./Card"
 
 export class Player extends Schema{
     
-    private hand : Hand;
+    protected _id:string;
+    protected _hand : Hand;
 
-    constructor(private serverClient: Client)
+    constructor()
     {
         super();
-        this.hand=new Hand();
+        this._hand=new Hand();
     }
-
-    public get client(){
-        return this.serverClient;
-    }
+    public get id(){
+        return this._id;
+    } 
 
     public addCard(card:Card) : void{
-        this.hand.addCard(card);
+        this._hand.addCard(card);
     }
 
     public removeCard(key:number): Card
     {
         let removedCard=this.getCard(key);
-        this.hand.removeCard(key);
+        this._hand.removeCard(key);
         return removedCard;
     }
 
     public swapCard(card:Card,index:number):Card
     {
-        return this.hand.swapCard(card,index);
+        return this._hand.swapCard(card,index);
     }
 
     public getCard(key:number){
-        return this.hand.getCard(key);
+        return this._hand.getCard(key);
     }
 
     public getPoints() : number
     {
-        return this.hand.val;
+        return this._hand.val;
     }
 
     public toString() :string {
-        return this.serverClient.sessionId+": "+this.hand.toString();
+        return this.id+": "+this._hand.toString();
     }
 
 }
